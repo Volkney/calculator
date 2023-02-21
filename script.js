370dc1bcc0f8f9225308ef19deb95bfc1b1c87eb
Volkney <jorge.24.co@gmail.com>
2023-02-21 15:17:35 -0600

    added clear functionality by eventlistener to variable clear

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let buttons = document.querySelectorAll('[id^="btn"]');
 let display = document.getElementsByClassName('display')[0];
+let clear = document.getElementById('clearAll');
+
 
 buttons.forEach(button => {
     button.addEventListener('click', function(){
         console.log(button.value);
         display.textContent += button.value;
     });
+});
+
+clear.addEventListener('click', function(){
+    display.textContent = '';
 });
